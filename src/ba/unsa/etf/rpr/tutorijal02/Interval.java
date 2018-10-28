00280e0b2f0013d4ddf14058ecc415f9f53903f7
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-10-28 02:26:52 +0200

    Definisani atributi intervala

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -2,5 +2,12 @@ package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
 
+    private double pocetnaTacka;
+    private double krajnjaTacka;
+    private boolean pocetnaPripada;
+    private boolean krajnjaPripada;
+
+    
+
 
 }

26319a362c8329dc969d183ffca693e4bb129dff
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-10-28 02:27:33 +0200

    Definisan glavni konstruktor

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -7,6 +7,18 @@ public class Interval {
     private boolean pocetnaPripada;
     private boolean krajnjaPripada;
 
+    Interval(double pocetna, double krajnja, boolean pocetnaPri, boolean krajnjaPri) {
+        if (pocetna > krajnja)
+            throw new IllegalArgumentException("Pocetna tacka je veca od krajnje");
+        else if (Double.compare(pocetna, krajnja) == 0 && pocetnaPri != krajnjaPri)
+            throw new IllegalArgumentException("Tacka pripada i ne pripada intervalu");
+
+        pocetnaTacka = pocetna;
+        pocetnaPripada = pocetnaPri;
+        krajnjaTacka = krajnja;
+        krajnjaPripada = krajnjaPri;
+    }
+
     
 
 
